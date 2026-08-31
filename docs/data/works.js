c9180c328a18279b5f12f7f4aaa1d2a8960028c9
Jieun Kim <metaphor3021@gmail.com>
2026-08-31 23:29:58 +0200

    Update works.js

--- docs/data/works.js
+++ docs/data/works.js
@@ -1,5 +1,35 @@
 const works =[
     {
+        {
+    id: -2,
+    show: true,
+    title: "MANUAL LIVING",
+    material: "paper, potassium silicate, concrete",
+    year: "2026",
+    link: "manualLiving.html",
+
+    thumbnail: "manualLiving_thumb.jpg",
+    aspectRatio: "16/9",
+
+    showOnMain: true,
+    orderOnMain: 1,
+    mainThumbnail: "manualLiving_main.jpg",
+},
+{
+    id: -1,
+    show: true,
+    title: "VISUAL SILENCE",
+    material: "discarded paper, hanji, natural lacquer, LED",
+    year: "2026",
+    link: "visualSilence.html",
+
+    thumbnail: "visualSilence_thumb.jpg",
+    aspectRatio: "3/4",
+
+    showOnMain: true,
+    orderOnMain: 2,
+    mainThumbnail: "visualSilence_main.jpg",
+},
         id: 1,  // Work 페이지에서 보이는 순서 (낮을수록 먼저 보임)
         show: true, // Work 페이지에서 보이는 여부
         title: "PAPER LAMP III", // 작품 제목
@@ -10,7 +40,7 @@ const works =[
         thumbnail: "paperLamp3_thumb.jpg", // Work 페이지에서 보이는 썸네일 이미지 파일명
         aspectRatio: "1/1", // Work 썸네일 이미지의 가로세로 비율 (예: 1/1, 3/4, 16/9 등)
 
-        showOnMain: true, // 메인 페이지에서 보이는 여부
+        showOnMain: false, // 메인 페이지에서 보이는 여부
         orderOnMain: 1, // 메인 페이지에서 보이는 순서 (낮을수록 먼저 보임)
         mainThumbnail: "paperLamp3_main.jpg", // 메인 페이지 썸네일 이미지 파일명
     },
@@ -25,7 +55,7 @@ const works =[
         thumbnail: "inkDyedPaperTable_thumb.jpg",
         aspectRatio: "3/4",
 
-        showOnMain: true,
+        showOnMain: false,
         orderOnMain: 3,
         mainThumbnail: "inkDyedPaperTable_main.jpg",
     },
@@ -41,7 +71,7 @@ const works =[
         aspectRatio: "3/4",
 
         showOnMain: false,
-        orderOnMain: 0,
+        orderOnMain: 4,
         mainThumbnail: "",
     },
     {
@@ -87,7 +117,7 @@ const works =[
         aspectRatio: "3/4",
 
         showOnMain: true,
-        orderOnMain: 2,
+        orderOnMain: 3,
         mainThumbnail: "afterClay_main.jpg",
     },
     {
